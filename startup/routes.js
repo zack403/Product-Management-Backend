e4a8dbc113e547b7490add20cfc6dc036fa5d3f9
Zack <aminuzack7@gmail.com>
2021-01-17 05:44:56 +0100

    deployment 3

--- startup/routes.js
+++ startup/routes.js
@@ -1,23 +1,10 @@
 const express = require('express');
-const auth = require('../controllers/auth.controller');
-const user = require('../controllers/user.controller');
-const package = require('../controllers/package.controller');
-const investment = require('../controllers/investment.controller');
-const purchase = require('../controllers/purchase.controller');
-const dashboard = require('../controllers/dashboard.controller');
-const utility = require('../controllers/utility.controller');
-const FoodMarket = require('../controllers/FoodMarket.controller');
+const product = require('../controllers/product.controller');
+
 const errorHandler = require('../middlewares/error');
 
 module.exports = app => {
   app.use(express.json());
-  app.use('/api/v1/auth', auth);
-  app.use('/api/v1/users', user);
-  app.use('/api/v1/package', package);
-  app.use('/api/v1/purchase', purchase);
-  app.use('/api/v1/investment', investment);
-  app.use('/api/v1/dashboard', dashboard);
-  app.use('/api/v1/utility', utility);
-  app.use('/api/v1/foodmarket', FoodMarket);
+  app.use('/api/v1/product', product)
   app.use(errorHandler);
 }
